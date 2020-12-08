5ff0482577d4e772cce3e45a9cf4bc155f88e1e1
lorihenderson <57110731+lorihenderson@users.noreply.github.com>
2020-12-08 15:37:50 -0500

    Initialize gh-pages

---
+++ main.js
@@ -0,0 +1,9 @@
+// Your Code Here!
+
+// Create a function that "rolls a die" and give you a number 1-6.
+
+// Then roll two dice and add them together.
+
+// Do that 1000 times and create an array of how frequently each result occurrs.
+
+// Then render the output to the HTML page.
